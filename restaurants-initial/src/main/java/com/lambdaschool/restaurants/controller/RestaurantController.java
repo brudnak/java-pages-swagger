8e174fa84690a30b94f376eec9e0ec6709ddfa17
Andrew Brudnak <brudnak@icloud.com>
2019-07-23 09:20:10 -0700

    :fire: Setting up pages for restaurants

--- restaurants-initial/src/main/java/com/lambdaschool/restaurants/controller/RestaurantController.java
+++ restaurants-initial/src/main/java/com/lambdaschool/restaurants/controller/RestaurantController.java
@@ -3,6 +3,7 @@ package com.lambdaschool.restaurants.controller;
 import com.lambdaschool.restaurants.model.Restaurant;
 import com.lambdaschool.restaurants.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,7 +25,8 @@ public class RestaurantController
 
     @GetMapping(value = "/restaurants",
                 produces = {"application/json"})
-    public ResponseEntity<?> listAllRestaurants()
+    // Adding parameter
+    public ResponseEntity<?> listAllRestaurants(Pageable pageable)
     {
         List<Restaurant> myRestaurants = restaurantService.findAll();
         return new ResponseEntity<>(myRestaurants, HttpStatus.OK);
